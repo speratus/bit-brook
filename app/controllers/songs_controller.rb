2226ebaddc04e8ac826cb26d0d8ff43658c7dfec
Andrew Luchuk <spaxumof@gmail.com>
2020-09-26 16:26:10 -0400

    updates new song page to allow song submission

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,40 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.album = Album.new
+    @song.artists << Artist.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    artist_name = song_params[:artists_attributes]['0'][:name]
+
+    artist = Artist.find_by(name: artist_name)
+
+    unless artist.nil?
+      @song.artists = [artist]
+    end
+
+    album_title = song_params[:album_attributes][:title]
+    album = Album.find_by(title: album_title)
+
+    unless album.nil?
+      @song.album = album
+    end
+
+    if @song.save
+      redirect_to songs_path
+    else
+      redirect_to new_song_path
+    end
+
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :length, artists_attributes: [:name], album_attributes: [:title])
   end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,6 @@ class Song < ApplicationRecord
 
   has_many :song_artists
   has_many :artists, through: :song_artists
+
+  accepts_nested_attributes_for :artists, :album
 end
