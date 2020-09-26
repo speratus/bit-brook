9aeee904099326bdf80c9debaff0a8dda57d1b02
Andrew Luchuk <spaxumof@gmail.com>
2020-09-26 18:51:25 -0400

    gets Vue displaying in the site controller index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :songs, only: [:index, :new, :create]
+
+  resources :site, only: :index
 end
