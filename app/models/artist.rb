05e36379374ae3df45c1fd4a53e3b6e731782c2d
Andrew Luchuk <spaxumof@gmail.com>
2020-09-26 14:55:02 -0400

    adds song demo song list page.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ApplicationRecord
   has_many :song_artists
-  has_many :songs
+  has_many :songs, through: :song_artists
 
   has_many :album_artists
-  has_many :albums
+  has_many :albums, through: :album_artists
 end
